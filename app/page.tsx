55def61d4950df5b83bddc23612c0b246048f8c7
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-01-22 01:37:08 +0500

    Authenthication setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-        <Button size="lg" variant="destructive" className="">
-          Click Me
-        </Button>
+    <div className="flex flex-col gap-y-4 ">
+      <h1>Dash Board</h1>
+        <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
