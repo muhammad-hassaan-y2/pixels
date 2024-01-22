1a150a01fa0753d3d999a9fe41fd580573b31533
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-01-23 00:50:13 +0500

    Dark Theme Mode Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from "@clerk/themes"
+
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            storageKey="Pixels Gaming Theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
     </ClerkProvider>
   );
